1fcf8d1cd5d1c5aad29f2340d2fd6f79c98301a1
Lintes <btvirtuoso@yeah.net>
2019-07-17 13:00:14 +0800

    update

--- init.el
+++ init.el
@@ -30,15 +30,3 @@
 (require 'init-appearance)
 
 ;;用户配置文件，自动生成
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode company monokai-theme))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
