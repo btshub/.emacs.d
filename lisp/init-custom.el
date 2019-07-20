17d6f6e4f73dc3af005bd9ad4e641d24ecfaaeb8
lintes <btvirtuoso@yeah.net>
2019-07-20 18:51:29 +0800

    set default custom.el file as init-custom.el

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -1,18 +1,4 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
+ ;; the code here will add by emacs automatically
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet monokai-theme exec-path-from-shell evil-visual-mark-mode company-lsp company-go))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-scrollbar-bg ((t (:background "cyan"))))
- '(company-scrollbar-fg ((t (:background "white"))))
- '(company-tooltip ((t (:background "cyan" :foreground "blue"))))
- '(company-tooltip-common ((t (:foreground "magenta"))))
- '(company-tooltip-selection ((t (:background "yellow")))))
