644eddce42717d324cbdbb28bcfc96a0e447bd57
lintes <btvirtuoso@yeah.net>
2019-07-23 00:16:47 +0800

    update

--- lisp/init-custom.el
+++ lisp/init-custom.el
@@ -2,3 +2,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (evil yasnippet company-lsp lsp-mode go-mode company monokai-theme))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "cyan"))))
+ '(company-scrollbar-fg ((t (:background "white"))))
+ '(company-tooltip ((t (:background "cyan" :foreground "blue"))))
+ '(company-tooltip-common ((t (:foreground "magenta"))))
+ '(company-tooltip-selection ((t (:background "yellow")))))
